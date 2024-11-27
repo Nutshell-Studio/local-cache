9654afe5afb48cffd708fe8572cd3949c2addc68
ns-wiz <124878636+ns-wiz@users.noreply.github.com>
2024-11-27 23:26:39 +0800

    Refactor save function to improve cache handling

--- src/save/index.ts
+++ src/save/index.ts
@@ -3,23 +3,17 @@ import {exec} from '../utils/cache'
 
 async function run(): Promise<void> {
   try {
-    const cacheHit = core.getState('cache-hit')
     const key = core.getState('key')
 
-    if (cacheHit === 'false') {
-      const cachePath = core.getState('cache-path')
-      const path = core.getState('path')
+    const cachePath = core.getState('cache-path')
+    const path = core.getState('path')
 
-      await exec(`mkdir -p ${cachePath}`)
-      const mv = await exec(`mv ./${path} ${cachePath}`)
+    await exec(`mkdir -p ${cachePath}`)
+    const mv = await exec(`mv ./${path} ${cachePath}`)
 
-      core.debug(mv.stdout)
-      if (mv.stderr) core.error(mv.stderr)
-      if (!mv.stderr) core.info(`Cache saved with key ${key}`)
-    } else {
-      core.info(`Cache hit on the key ${key}`)
-      core.info(`,not saving cache`)
-    }
+    core.debug(mv.stdout)
+    if (mv.stderr) core.error(mv.stderr)
+    if (!mv.stderr) core.info(`Cache saved with key ${key}`)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
